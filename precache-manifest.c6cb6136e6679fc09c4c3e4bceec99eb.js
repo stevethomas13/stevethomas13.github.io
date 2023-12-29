b5a3b89a4fb8d2eb0d0fc8d6c5e76af07fd71169
Steve Thomas <steve13thomas@gmail.com>
2023-12-29 11:35:57 +0000

    Updates

---
+++ precache-manifest.c6cb6136e6679fc09c4c3e4bceec99eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "786472403fd10f16cf69304ddf93faa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/static/js/main.01b01939.chunk.js"
+  },
+  {
+    "revision": "cd2988c65fcca3ce3217",
+    "url": "/static/js/runtime~main.b45b9353.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
