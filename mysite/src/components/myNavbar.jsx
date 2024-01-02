00fcc1df998ab8239d22ae840652220e1ecf5642
Steve Thomas <steve13thomas@gmail.com>
2024-01-02 10:49:55 +0000

    removed about, added experience and education, remove about me

--- mysite/src/components/myNavbar.jsx
+++ mysite/src/components/myNavbar.jsx
@@ -13,9 +13,9 @@ function myNavbar() {
                         <Nav className="me-auto">
                             <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link href="#projects">Projects</Nav.Link>
-                            <Nav.Link href="#about">About</Nav.Link>
-                            {/* Contact should always be last because of scroll css */}
-                            <Nav.Link href="#contact">About</Nav.Link>
+                            <Nav.Link href="#education">Experience</Nav.Link>
+                            {/* Education should always be last because of scroll css */}
+                            <Nav.Link href="#experience">Education</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
