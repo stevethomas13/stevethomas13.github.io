0d8b132c60f9f044e1c2eb003daad10f248a8d8c
Steve Thomas <steve13thomas@gmail.com>
2024-01-02 11:03:30 +0000

    made navbar toggleable

--- mysite/src/components/myNavbar.jsx
+++ mysite/src/components/myNavbar.jsx
@@ -9,13 +9,14 @@ function myNavbar() {
         <div>
             <Navbar expand="lg" className="bg-body-tertiary">
                 <Container>
+                    <Navbar.Brand href="">Steve Thomas</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link href="#projects">Projects</Nav.Link>
-                            <Nav.Link href="#education">Experience</Nav.Link>
+                            <Nav.Link href="#experience">Experience</Nav.Link>
                             {/* Education should always be last because of scroll css */}
-                            <Nav.Link href="#experience">Education</Nav.Link>
+                            <Nav.Link href="#education">Education</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
