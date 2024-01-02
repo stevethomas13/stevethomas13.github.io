b1365bb42f5da92e1ef0b4b97407b76056a2ff7c
Steve Thomas <steve13thomas@gmail.com>
2024-01-02 10:14:40 +0000

    updated information

--- mysite/src/pages/About.jsx
+++ mysite/src/pages/About.jsx
@@ -1,110 +1,48 @@
-import React  from 'react';
-// import {Table} from 'react-bootstrap'
-// import { Container } from 'react-bootstrap';
-// import {Row} from 'react-bootstrap';
-// import {Col} from 'react-bootstrap'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap'
 
 export default function About() {
     return (
-        <div>
-            <p>hello world</p>
+        <div id="about">
+            <Container fluid style={{ textAlign: "center" }}>
+                <Row className="justify-content-md-center">
+                    <Col xs lg="6">
+                        <h1 classname="display-1">About me!</h1>
+                    </Col>
+                </Row>
+                <Row className="justify-content-md-center">
+                    <Col xs lg="6">
+                        <h3>Education</h3>
+                        <p>
+                            I'm a Computer Science and Business student at Trinity, currently delving into the dynamic intersection of technology and business. In my second year, I completed a NFQ Level 7 course in Innovation and Entrepreneurship, fueling my passion for creative problem-solving and strategic thinking.
+                        </p>
+                    </Col>
+                </Row>
+                <Row className="justify-content-md-center">
+                    <Col xs lg="6">
+                        <h3>Experience</h3>
+                        <p><b>Musgrave Group</b></p>
+                        <p>
+                            In my role as a Supply Chain Project Manager at Musgrave, I successfully led the Recoup Process at the CDC Blanchardstown depot, achieving an impressive jump in recoup rates from 35% to 83%. This not only resulted in substantial annual savings but also showcased my ability to excel under budget constraints and tight deadlines.
+                        </p>
+                        <p><b>Formula Trinity</b></p>
+                        <p>
+                            At Formula Trinity, my focus on financial management and strategic sponsorships honed my skills in budgeting, financial planning, and relationship building.
+                        </p>
+                        <p><b>ProtexAI</b></p>
+                        <p>
+                        As a junior consultant with the Irish Student Consulting Group, I collaborated with ProtexAI, contributing to their online visibility and market reach through effective SEO strategies.
+                        </p>
+                    </Col>
+                </Row>
+
+                
+
+            </Container>
+
         </div>
-        // <div>
-        //     <Container fluid style={{textAlign:"center"}}>
-        //         <Row className="justify-content-md-center">
-        //             <Col xs lg="6">
-        //                 <p>hello</p>
-        //             </Col>
-        //         </Row>
-        //         <br>
-        //         </br>
-        //         <h2>Project Overview</h2>
-        //         <Row className="justify-content-md-center">
-        //             <Col xs lg="8">
-        //                 <Table striped bordered hover>
-        //                     <thead>
-        //                     <tr>
-        //                         <th>#</th>
-        //                         <th>First Name</th>
-        //                         <th>Last Name</th>
-        //                         <th>Team</th>
-        //                     </tr>
-        //                     </thead>
-        //                     <tbody>
-        //                     <tr>
-        //                         <td>1</td>
-        //                         <td>Andrew</td>
-        //                         <td>Syomushkin</td>
-        //                         <td>FrontEnd + Docs</td>
-        //                     </tr>
-        //                     <tr>
-        //                         <td>2</td>
-        //                         <td>Steve</td>
-        //                         <td>Thomas</td>
-        //                         <td>FrontEnd + BackEnd </td>
-        //                     </tr>
-        //                     <tr>
-        //                         <td>3</td>
-        //                         <td>Jake</td>
-        //                         <td>McKenna</td>
-        //                         <td>FrontEnd + BackEnd</td>
-        //                     </tr>
-        //                     <tr>
-        //                         <td>3</td>
-        //                         <td>Micheal</td>
-        //                         <td>Keogh</td>
-        //                         <td>FrontEnd</td>
-        //                     </tr>
-        //                     <tr>
-        //                         <td>3</td>
-        //                         <td>Yuheng</td>
-        //                         <td>Ye</td>
-        //                         <td>BackEnd</td>
-        //                     </tr>
-        //                     <tr>
-        //                         <td>3</td>
-        //                         <td>Yuuv</td>
-        //                         <td>Jauhari </td>
-        //                         <td>BackEnd + Docs</td>
-        //                     </tr>
-        //                     </tbody>
-        //                 </Table>
-        //             </Col>
-        //         </Row>
-
-        //         <div classname="about-container">
-        //             <div className="text-container">
-        //                 <h2>About The project</h2>
-        //                 <p>
-        //                     Our team, Group 11, integrated React for the frontend and Django for the backend. We used these technologies as we were familiar with them. It doesn't hurt that combination of of these technologies gives our application a robust and efficient architecture.
-        //                 </p>
-        //                 <p>
-        //                     In the sprint 1, our focus was on planning and designing. We began by creaeting detailed mockups to help visualize the desired appearance and functionality of our application.
-        //                     At the same time, we wrote user stories, setting the foundation for our development roadmap.
-        //                 </p>
-        //                 <p>On the backend, we employed Django, a Python web framework known for its versatility and scalability. We used Django facilitate the development of our secure and maintainable web applications. Django follows the Model-View-Controller architectural pattern we learned about in lectures, to ensure a well-organized and structured backend.</p>
-        //                 <p>
-        //                     It's worth noting that Django utilizes a SQL database for data storage. This relational database management system ensures data integrity and consistency, crucial for our application's reliability.
-
-        //                 </p>
-        //                 <p>
-        //                     Our project leverages the CocktailDB API to enrich the user experience. This external API allows us to access a large database of cocktail-related information, instilling our application with a diverse range of drink options and details.
-        //                 </p>
-        //                 <p>
-        //                     Within the first sprint, we made great progress in implementing core functionalities.
-        //                     We successfully implemented a search bar, random cocktail generator, and list display functionalities. We also prioritized user management features, implementing user registration, login, and logout functionalities.
-        //                 </p>
-        //                 <p>
-        //                     This rapid development of core dunctionalities allowed us to designate more time in sprint 2 for implementation of our functionalities to the frontend. We discussed and tried many bootstrap implementations until we settled on this one.
-        //                 </p>
-        //                 <p>
-        //                     We hope you enjoyed this project as much as we enjoyed creating it!
-        //                 </p>
-        //             </div>
-        //         </div>
-        //     </Container>
-
-        // </div>
     );
 };
 
