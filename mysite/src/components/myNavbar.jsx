a53d037e8fa3dc34439ef4dd7256fcd83d29fdc9
Steve Thomas <steve13thomas@gmail.com>
2024-01-02 10:41:16 +0000

    added contact page

--- mysite/src/components/myNavbar.jsx
+++ mysite/src/components/myNavbar.jsx
@@ -1,22 +1,28 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import React  from 'react';
+import React from 'react';
+import { Outlet } from "react-router-dom";
 
 function myNavbar() {
-  return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="about">About</Nav.Link>
-            <Nav.Link href="projects">Projects</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+    return (
+        <div>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="#projects">Projects</Nav.Link>
+                            <Nav.Link href="#about">About</Nav.Link>
+                            {/* Contact should always be last because of scroll css */}
+                            <Nav.Link href="#contact">About</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Outlet />
+        </div>
+    );
 }
 
 export default myNavbar;
