438231f7cc72921551c18b06773652cffe286d66
Steve Thomas <steve13thomas@gmail.com>
2024-01-03 20:49:05 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b75df3549e2fe773018d60f223a4db23.js"
+  "/precache-manifest.9cfe8c550499269f5a4d281904c4ff4d.js"
 );
 
 self.addEventListener('message', (event) => {
